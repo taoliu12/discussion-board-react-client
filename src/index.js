91748735927203b5bab4f4579ba10817f244dfa7
taoliu12 <taoliudesign@gmail.com>
2023-10-23 11:25:23 -0700

    fix webpack error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./components/reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./components/UserContext";
-import 'dotenv/config';
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -21,8 +19,3 @@ root.render(
     </React.StrictMode>
   </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
